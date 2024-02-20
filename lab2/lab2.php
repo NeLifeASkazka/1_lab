a5de30a6ae4cf1beeb01b374590d1a817d4d8587
NeLifeASkazka <110575273+NeLifeASkazka@users.noreply.github.com>
2024-02-20 10:14:58 +0200

    lab2

---
+++ lab2/lab2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Artur Rozhok</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="assets/css/my-styles.css">
+
+	<body>
+		<header id="header" class="header">
+			<div class="container"> 
+				<p class="story">Story</p>
+				<div class="top-bar">
+					<div class="block">
+						<p class="marcy">Marcy...</p>
+					</div>
+				</div>
+				<div class="nav-menu">
+					<ul class="menu">
+						<li class="link">li 1</li>
+						<li class="link">li 2</li>
+						<li class="link">li 3</li>
+					</ul>
+				</div>
+			</div>
+		</header>
+	</body>
+
+
+</head>
